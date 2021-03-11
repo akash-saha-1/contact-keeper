ecd8e980c9496e4c643c64b9d7e9e3053145efcb
akash-saha-1 <aakashsaha5thjuly@gmail.com>
2021-03-12 01:45:21 +0530

    port changed

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
   );
 }
 
-const PORT = process.env.port || 5000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server started on port ${PORT}`);
 });
