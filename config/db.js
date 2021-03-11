2ff693fbf7f0af83f835071d7a4df89b85b4c713
akash-saha-1 <aakashsaha5thjuly@gmail.com>
2021-03-12 00:06:29 +0530

    db changed

--- config/db.js
+++ config/db.js
@@ -4,31 +4,18 @@ const db = config.get("mongoURI");
 
 const connectDB = async () => {
   console.log(db);
-  // try {
-  //   await mongoose.connect(db, {
-  //     useNewUrlParser: true,
-  //     useCreateIndex: true,
-  //     useFindAndModify: false,
-  //     useUnifiedTopology: true,
-  //   });
-  //   console.log("MongoDb connected");
-  // } catch (error) {
-  //   console.error("MongoDb connection Error due to :" + error.message);
-  //   process.exit(1); // exit with failure
-  // }
-  mongoose
-    .connect(db, {
+  try {
+    await mongoose.connect(db, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false,
       useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log("Database connected successfully!");
-    })
-    .catch((err) => {
-      console.log("Error connecting with error code:", err);
     });
+    console.log("MongoDb connected");
+  } catch (error) {
+    console.error("MongoDb connection Error due to :" + error.message);
+    process.exit(1); // exit with failure
+  }
 };
 
 module.exports = connectDB;
