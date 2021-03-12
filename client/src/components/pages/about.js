98adefc2740c09215b9bc4f0ffdbb83aa9728058
akash-saha-1 <aakashsaha5thjuly@gmail.com>
2021-03-12 22:27:21 +0530

    about changed

--- client/src/components/pages/about.js
+++ client/src/components/pages/about.js
@@ -4,7 +4,10 @@ const About = () => {
   return (
     <div>
       <h1>About this App</h1>
-      <p className="my-1">This is a full stack React app for keeping contact</p>
+      <p className="my-1">
+        This is a Contact Keeper website. Which will save your contacts
+        securely.
+      </p>
       <p className="bg-dark p">
         <strong>Version : 1.0.0</strong>
       </p>
